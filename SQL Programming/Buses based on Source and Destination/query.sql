b1743ec73fff5c52fba4c13e483d8fe1ef448554
Nandheswara <70500938+Nandheswara@users.noreply.github.com>
2022-03-15 00:00:35 +0530

    Update query.sql

--- SQL Programming/Buses based on Source and Destination/query.sql
+++ SQL Programming/Buses based on Source and Destination/query.sql
@@ -1,16 +1,12 @@
-select distinct s1.bus_no,
-    b.bus_name
+select distinct s1.bus_no, b.bus_name
 from schedule s1
     inner join schedule s2 on s1.source = s2.destination
     and s1.destination = s2.source
     inner join buses b on s1.bus_no = b.bus_no
 order by s1.bus_no;
 
-select distinct s1.bus_no,
-    b.bus_name
-from schedule s1,
-    schedule s2,
-    buses b
+select distinct s1.bus_no, b.bus_name
+from schedule s1, schedule s2, buses b.bus_name
 where s1.source = s2.destination
     and s1.destination = s2.source
     and s1.bus_no = b.bus_no
