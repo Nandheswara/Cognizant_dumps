a9274719d5f632d97a3f19a9869f46c203b06531
Nandheswara <70500938+Nandheswara@users.noreply.github.com>
2022-03-23 11:49:06 +0530

    Delete Java Programming Fundamentals directory

--- Java Programming Fundamentals/Shape - Area Volume Calculation/Shape.java
+++
@@ -1,5 +0,0 @@
-public abstract class Shape {
-    public abstract double area();
-
-    public abstract double volume();
-}
--- Java Programming Fundamentals/Vehicle Loan Insurance - Use Interface/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Vehicle v = new Vehicle("WB2335", "Hero Glamour Plus", "2 wheeler", 80000);
-        System.out.println(v.issueLoan());
-        System.out.println(v.takeInsurance());
-    }
-}
